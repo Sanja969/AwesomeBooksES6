dd9db03093674cdcf544f7e539504096c070033c
Sanja Mandic <sanja1mandic@gmail.com>
2022-05-30 13:11:29 +0200

    put luxon.js to be ignored and change var f to be const f in time module

--- modules/currentTime.js
+++ modules/currentTime.js
@@ -3,8 +3,7 @@ import { DateTime } from "../luxon.js";
 const showCurrentTime = () => {
   const time = document.querySelector('.time');
   const now = DateTime.now();
-  const n = 'numeric';
-  var f = DateTime.DATETIME_FULL_WITH_SECONDS;
+  const f = DateTime.DATETIME_FULL_WITH_SECONDS;
   const date = now.setLocale('en-US').toLocaleString(f);
   time.textContent = date;
 }

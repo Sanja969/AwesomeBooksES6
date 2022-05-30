694711cf84b4d1dc5a032b95f32bf3b80841de64
Sanja Mandic <sanja1mandic@gmail.com>
2022-05-30 14:20:07 +0200

    use local path instead of online for luxon

--- modules/currentTime.js
+++ modules/currentTime.js
@@ -1,4 +1,4 @@
-import { DateTime } from 'https://moment.github.io/luxon/es6/luxon.js';
+import { DateTime } from '../luxon.js';
 
 const showCurrentTime = () => {
   const time = document.querySelector('.time');

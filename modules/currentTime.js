e8bb29e201b0ee7f06d62d84480fb951b2104d50
Sanja Mandic <sanja1mandic@gmail.com>
2022-05-30 14:06:02 +0200

    use online luxon link insted of local

--- modules/currentTime.js
+++ modules/currentTime.js
@@ -1,4 +1,4 @@
-import { DateTime } from '../luxon.js';
+import { DateTime } from 'https://moment.github.io/luxon/es6/luxon.js';
 
 const showCurrentTime = () => {
   const time = document.querySelector('.time');
